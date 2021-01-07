35ebde6dc192a895b6eaa7e7fca6acbc2d154ba8
douknowccy <twkccy1990@gmail.com>
2021-01-07 21:38:31 +0800

    first commit

---
+++ src/action.tsx
@@ -0,0 +1,5 @@
+export const INCREASE_LEVEL = "INCREASE_LEVEL";
+export type INCREASE_LEVEL = typeof INCREASE_LEVEL;
+
+export const DECREASE_LEVEL = "DECREASE_LEVEL";
+export type DECREASE_LEVEL = typeof DECREASE_LEVEL;
